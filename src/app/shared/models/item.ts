9c4d604b8d4cec13afa7286f48f2b4bba8f0a1ea
Carolina <caroliina20003@gmail.com>
2022-05-09 19:11:49 -0500

    Se elimina clase interface Item innecesaria

--- src/app/shared/models/item.ts
+++ src/app/shared/models/item.ts
@@ -1,10 +1,4 @@
-import { IAuthor } from "./author"
 import { IPrice } from "./price"
-
-export interface IItem {
-    author?: IAuthor,
-    item: IItemDetail
-}
 
 export interface IItemDetail {
     id: string,
